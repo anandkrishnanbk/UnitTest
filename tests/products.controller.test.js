79b89895fddc4b1fd536c8263af6fc6629ccc727
anandkrishnankutty <anand.krishnankutty@bcbsnc.com>
2016-02-09 15:59:12 -0500

    added

--- tests/products.controller.test.js
+++ tests/products.controller.test.js
@@ -1,6 +1,6 @@
-describe('All products tests',function()
+describe('All products controller tests',function()
 {
-    var alias='prodApp';
+    /*var alias='prodApp';
     var obj={"customerService":{getcustIds:function()
     {
         return [23,34,56,76];
@@ -10,5 +10,13 @@ describe('All products tests',function()
     {
         $provide.constant('discountPercent',0.10);
     };
-    beforeEach(module(alias,obj,func))
+    beforeEach(module(alias,obj,func))*/
+    var controller;
+    beforeEach(module('prodApp'));
+    beforeEach(inject(function(_controller_)
+    {
+        controller = _controller_;
+    }));
+    it('')
+
 });
